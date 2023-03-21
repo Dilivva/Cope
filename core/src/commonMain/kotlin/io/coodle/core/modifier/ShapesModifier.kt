e769936fe9882dfe22a2c9378f5b6d066982c21e
cherrio <cherrio.llc@gmail.com>
2023-03-21 14:25:59 +0100

    Implement some basic shapes
    1. RoundedCorner
    2. CircleShape
    3. CutCornerShape
    4. RectangleShape
    
    User can implement his/her own custom shape.

--- core/src/commonMain/kotlin/io/coodle/core/modifier/ShapesModifier.kt
+++ core/src/commonMain/kotlin/io/coodle/core/modifier/ShapesModifier.kt
@@ -2,36 +2,102 @@ package io.coodle.core.modifier
 
 import io.coodle.core.node.DoodleNode
 import io.nacular.doodle.core.*
+import io.nacular.doodle.drawing.Canvas
+import io.nacular.doodle.drawing.Stroke
+import io.nacular.doodle.drawing.paint
+import io.nacular.doodle.geometry.Circle
+import io.nacular.doodle.geometry.Point
+import io.nacular.doodle.geometry.path
+import io.nacular.doodle.geometry.rounded
 
 interface Clip{
     val radius: Double
-    fun getType(): ShapeType
-
-    enum class ShapeType{
-        Rectangle, Circle
-    }
-
-    companion object: Clip{
-        override val radius: Double = 0.0
-
-        override fun getType(): ShapeType = ShapeType.Rectangle
-
-    }
+    fun render(canvas: Canvas, doodleNode: DoodleNode)
 }
-class RoundedCorner(private val radii: Int = 0): Clip{
+class RoundedCorner(
+    private val topLeft: Int = 0,
+    private val topRight: Int = 0,
+    private val bottomLeft: Int = 0,
+    private val bottomRight: Int = 0
+): Clip{
     override val radius: Double
-        get() = radii.toDouble()
-    override fun getType(): Clip.ShapeType = Clip.ShapeType.Rectangle
+        get() = (topLeft + topRight + bottomLeft + bottomRight).toDouble()
+
+    override fun render(canvas: Canvas, doodleNode: DoodleNode) {
+        val paint = doodleNode.backgroundColor.paint
+        val stroke = Stroke.invoke(doodleNode.border.color, doodleNode.border.width.toDouble())
+        val path = doodleNode.view.bounds.atOrigin.rounded { index: Int, _: Point ->
+            return@rounded when(index){
+                0 -> topLeft.toDouble()
+                1 -> topRight.toDouble()
+                2 -> bottomRight.toDouble()
+                else -> bottomLeft.toDouble()
+            }
+        }
+        canvas.path(path, fill = paint, stroke = stroke)
+    }
 
     override fun toString(): String {
-        return "RoundedCorner(radius=$radii)"
+        return "RoundedCorner(radius=$radius)"
     }
 }
 
-class CircleShape(private val radii: Int): Clip{
+object RectangleShape: Clip{
+    override val radius: Double = 0.0
+    override fun render(canvas: Canvas, doodleNode: DoodleNode) {
+        val paint = doodleNode.backgroundColor.paint
+        val stroke = Stroke.invoke(doodleNode.border.color, doodleNode.border.width.toDouble())
+        canvas.rect(
+            doodleNode.view.bounds.atOrigin,
+            fill = paint,
+            stroke = stroke
+        )
+    }
+}
+
+object CircleShape: Clip{
     override val radius: Double
-        get() = radii.toDouble()
-    override fun getType() = Clip.ShapeType.Circle
+        get() = 0.0
+
+    override fun render(canvas: Canvas, doodleNode: DoodleNode) {
+        val paint = doodleNode.backgroundColor.paint
+        val stroke = Stroke.invoke(doodleNode.border.color, doodleNode.border.width.toDouble())
+        canvas.circle(
+            Circle( center = doodleNode.view.center,doodleNode.view.bounds.height / 2),
+            fill = paint,
+            stroke = stroke
+        )
+    }
+}
+class CutCornerShape(
+    private val topLeft: Int = 0,
+    private val topRight: Int = 0,
+    private val bottomLeft: Int = 0,
+    private val bottomRight: Int = 0
+): Clip{
+    override val radius: Double
+        get() = 0.0
+
+    override fun render(canvas: Canvas, doodleNode: DoodleNode) {
+        val paint = doodleNode.backgroundColor.paint
+        val stroke = Stroke.invoke(doodleNode.border.color, doodleNode.border.width.toDouble())
+
+        val startPosition = doodleNode.view.bounds.atOrigin.position
+        val startEdge = Point(x = startPosition.x + topLeft, startPosition.y )
+        val path = path(startEdge)
+        val rect = doodleNode.view.bounds.atOrigin
+
+        path.lineTo(Point(x = rect.width - topRight, startPosition.y))
+        path.lineTo(Point(x = rect.width, startPosition.y + topRight))
+        path.lineTo(Point(x = rect.width, rect.height - bottomRight))
+        path.lineTo(Point(x = rect.width - bottomRight, rect.height))
+        path.lineTo(Point(x = startPosition.x + bottomLeft, rect.height))
+        path.lineTo(Point(x = startPosition.x, rect.height - bottomLeft))
+        path.lineTo(Point(x = startPosition.x, startPosition.y + topLeft))
+        path.lineTo(startEdge)
+        canvas.path(path.close(), fill = paint, stroke = stroke)
+    }
+
 }
 
 fun Modifier.clip(clip: Clip): Modifier{
